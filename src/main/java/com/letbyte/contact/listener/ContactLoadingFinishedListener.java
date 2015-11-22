b228a15ae55e3fe640fb2bc2ad4ac967665f26aa
unknown <ahmedmohmmadullah@gmail.com>
2015-11-22 20:57:19 +0600

    Pull latest

--- src/main/java/com/letbyte/contact/listener/ContactLoadingFinishedListener.java
+++
@@ -1,8 +0,0 @@
-package com.letbyte.contact.listener;
-
-/**
- * Created by Max on 18-Oct-15.
- */
-public interface ContactLoadingFinishedListener {
-    void contactLoadingFinished();
-}
--- src/main/java/com/letbyte/contact/loader/Command.java
+++
@@ -1,5 +0,0 @@
-package com.letbyte.contact.loader;
-
-public interface Command {
-	void execute();
-}

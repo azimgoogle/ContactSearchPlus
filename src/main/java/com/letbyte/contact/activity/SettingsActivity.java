c44f77328bf9d7d77b9b5041f06096dd1853b068
ifte <ifte.net@gmail.com>
2015-12-13 11:46:55 +0600

    final 23 version

--- src/main/java/com/letbyte/contact/activity/SettingsActivity.java
+++ src/main/java/com/letbyte/contact/activity/SettingsActivity.java
@@ -4,18 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.letbyte.contact.R;
 import com.letbyte.contact.control.PrefManager;
 
-/*public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
+
+    private PrefFragment prefFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +23,59 @@ import com.letbyte.contact.control.PrefManager;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-      *//*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*//*
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        prefFragment = new PrefFragment();
+        getFragmentManager().beginTransaction().replace(R.id.contentFrame, prefFragment).commitAllowingStateLoss();
     }
 
-}*/
+    @Override
+    public void onBackPressed() {
+        prefFragment.onBackPressed();
+    }
 
+    public static final class PrefFragment
+            extends PreferenceFragment
+            implements SharedPreferences.OnSharedPreferenceChangeListener {
+        private boolean isChanged;
+
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+           // SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            PreferenceManager.getDefaultSharedPreferences(getActivity()).registerOnSharedPreferenceChangeListener(this);
+
+            addPreferencesFromResource(R.xml.settings);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            isChanged = true;
+        }
+
+        private void onBackPressed() {
+            if (isChanged) {
+                PrefManager.on(getActivity()).setSynced(false);
+                // new SyncTask(this.getBaseContext()).execute(PrefManager.on(this).getConfig());
+            }
+
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra("isChanged", isChanged);
+            getActivity().setResult(Activity.RESULT_OK, resultIntent);
+            getActivity().finish();
+        }
+
+    }
+}
+
+
+
+
+/*
 public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private boolean isChanged;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,4 +103,4 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         isChanged = true;
     }
-}
+}*/
